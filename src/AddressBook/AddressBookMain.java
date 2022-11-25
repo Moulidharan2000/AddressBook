32fb5a165459f3de7ea24a6773a114dc214f6b63
Moulidharan <116242759+Moulidharan2000@users.noreply.github.com>
2022-11-25 14:05:18 +0530

    add : Edit Contacts in AddressBook

--- src/AddressBook/AddressBookMain.java
+++ src/AddressBook/AddressBookMain.java
@@ -3,51 +3,7 @@ package AddressBook;
 import java.util.Scanner;
 
 public class AddressBookMain {
-
-    
-
-	@SuppressWarnings("unused")
 	public static void main(String[] args)  {
-		// TODO Auto-generated method stub
-		ContactPersonDetails person = new ContactPersonDetails();
-		Address address = new Address();
-		System.out.println("WELCOME TO ADDRESS BOOK PROGRAM");
-		
-		ContactPersonDetails person1 = new ContactPersonDetails();
-        Address address1 = new Address();
-        Scanner scannerObject = new Scanner(System.in);
-
-        System.out.print("Enter First Name : ");
-        String firstName = scannerObject.next();
-
-        System.out.print("Enter Last Name : ");
-        String lastName = scannerObject.next();
-        
-        System.out.print("Enter City : ");
-        String city = scannerObject.next();
-
-        System.out.print("Enter State : ");
-        String state = scannerObject.next();
-
-        System.out.print("Enter Zip Code: ");
-        long zipCode = scannerObject.nextLong();
-        
-        System.out.print("Enter Phone Number : ");
-        long phoneNumber = scannerObject.nextLong();
-
-        System.out.print("Enter Email : ");
-        String email = scannerObject.next();
-        
-        scannerObject.close();
-
-        person1.setFirstName(firstName);
-        person1.setLastName(lastName);
-        person1.setphoneNumber(phoneNumber);
-        person1.setEmail(email);
-        address1.setCity(city);
-        address1.setState(state);
-        address1.setZipCode(zipCode);
-        person1.setAddress(address1);
 
         AddressBook addressBook = new AddressBook();
         addressBook.operation();
